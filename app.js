83265bc4a3941236b2e5f36831388c7f5c75d521
Mikkel Ridley <mikkel250@gmail.com>
2019-04-23 17:49:49 -0700

    add routes and views

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-
+var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+var okta = require('@okta/okta-sdk-nodejs');
+var ExpressOIDC = require('@okta/oidc-middleware').ExpressOIDC;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const dashboardRouter = require("./routes/dashboard");
+const publicRouter = require("./routes/public");
 
 var app = express();
 
@@ -19,9 +21,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'B@G!rWc7RGm3^UmAEVL&g5svnIdu#!$qZzwza&cedQW%RpjFO&7F8hHi6dodwgZY0a7T6ZT2Z02fT8yzr80&g@e&vpLHUYUojwjr',
+  resave: true,
+  saveUninitialized: false
+}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', publicRouter);
+app.use('/dashboard', dashboardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
