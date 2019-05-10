9248699de3a854b7de4e3d78b512e455c0ad94d2
Mikkel Ridley <mikkel250@gmail.com>
2019-05-09 18:25:40 -0700

    try to solve username

--- app.js
+++ app.js
@@ -7,18 +7,47 @@ var session = require('express-session');
 var okta = require('@okta/okta-sdk-nodejs');
 var ExpressOIDC = require('@okta/oidc-middleware').ExpressOIDC;
 
+//const auth = require("./auth");
+//const middleware = require("./middleware");
+
 const dashboardRouter = require("./routes/dashboard");
 const publicRouter = require("./routes/public");
+const usersRouter = require("./routes/users");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+// Middleware
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "assets")));
+
+var oktaClient = new okta.Client({
+  orgUrl: '{https://dev-912173.okta.com}',
+  token: '{00MLeIKtlDehK7XDvBHVX3FlteIZCJH8aaAS3SM8pp}'
+});
+
+const oidc = new ExpressOIDC({
+  issuer: "https://dev-912173.okta.com/oauth2/default",
+  client_id: '0oahwz96t6lGCya5B356',
+  client_secret: 'PnEQDX5Fb1sktCIh4sw21Kr8XEO3M7mBOcwO0lxP',
+  redirect_url: 'http://localhost:3000/users/callback',
+  scope: 'openid profile',
+  routes: {
+    login: {
+      path: '/users/login',
+    },
+    callback: {
+      path: 'users/callback',
+      defaultRedirect: '/dashboard'
+    }
+  }
+});
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
@@ -27,8 +56,29 @@ app.use(session({
   saveUninitialized: false
 }));
 
-app.use('/', publicRouter);
-app.use('/dashboard', dashboardRouter);
+app.use(oidc.router);
+app.use((req, res, next) => {
+  if (!req.userinfo) {
+    return next();
+  }
+  oktaClient.getUser(req.userinfo.sub)
+    .then(user => {
+      req.user = user;
+      res.locals.user = user;
+      next();
+    }).catch(err => {
+      next(err);
+    });
+});
+
+function loginRequired(req, res, next) {
+  if (!req.user) {
+    return res.status(401).render("unauthenticated");
+  }
+  next();
+};
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+//Log a user out
+router.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+});
